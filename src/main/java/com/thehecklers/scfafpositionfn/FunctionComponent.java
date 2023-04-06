a7c27b05c3b749e4c12d54846439ff2f90cf79be
Mark Heckler <mark.heckler@gmail.com>
2023-04-05 20:27:36 -0400

    Initial commit of SCF native binary + OCI cloud native buildpack image

--- src/main/java/com/thehecklers/scfafpositionfn/FunctionComponent.java
+++ src/main/java/com/thehecklers/scfafpositionfn/FunctionComponent.java
@@ -1,30 +1,30 @@
 package com.thehecklers.scfafpositionfn;
 
-import com.microsoft.azure.functions.*;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
-import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
-import org.springframework.stereotype.Component;
+//import com.microsoft.azure.functions.*;
+//import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+//import com.microsoft.azure.functions.annotation.FunctionName;
+//import com.microsoft.azure.functions.annotation.HttpTrigger;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Optional;
+//import java.util.function.Function;
 
-import java.util.Optional;
-import java.util.function.Function;
-
-@Component
+//@Component
 public class FunctionComponent {
-    private Function<Position, Position> pos;
-
-    public FunctionComponent(Function<Position, Position> pos) {
-        this.pos = pos;
-    }
-
-    @FunctionName("pos")
-    public HttpResponseMessage pos(@HttpTrigger(name = "req",
-            methods = {HttpMethod.GET, HttpMethod.POST},
-            authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<Position>> request,
-                                   ExecutionContext context) {
-        return request.createResponseBuilder(HttpStatus.OK)
-                .header("Content-Type", "application/json")
-                .body(pos.apply(request.getBody().get()))
-                .build();
-    }
+//    private Function<Position, Position> pos;
+//
+//    public FunctionComponent(Function<Position, Position> pos) {
+//        this.pos = pos;
+//    }
+//
+//    @FunctionName("pos")
+//    public HttpResponseMessage pos(@HttpTrigger(name = "req",
+//            methods = {HttpMethod.GET, HttpMethod.POST},
+//            authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<Position>> request,
+//                                   ExecutionContext context) {
+//        return request.createResponseBuilder(HttpStatus.OK)
+//                .header("Content-Type", "application/json")
+//                .body(pos.apply(request.getBody().get()))
+//                .build();
+//    }
 }
